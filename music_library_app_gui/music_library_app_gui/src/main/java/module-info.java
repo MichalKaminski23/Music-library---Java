2c5faed7db818ec882710b2826c9f982fcf0db94
Michał <114977878+MichalKaminski23@users.noreply.github.com>
2024-10-27 17:26:51 +0100

    Delete music_library_app_gui/music_library_app_gui directory
    old

--- music_library_app_gui/music_library_app_gui/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.music_library_app_gui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.music_library_app_gui to javafx.fxml;
-    exports com.mycompany.music_library_app_gui;
-    exports com.mycompany.controllers;
-}
